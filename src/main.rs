a6bd0060c97923a26af3a85409f6395ab9273529
Crystal Kate <crystalnyan@icloud.com>
2022-11-04 09:56:08 +0500

    Update main.rs
    add translate endpoint to nyaaa~ language

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, post};
 
 #[get("/")]
 async fn home() -> impl Responder {
@@ -10,6 +10,17 @@ async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}!")
 }
 
+#[post("/translate")]
+async fn translate(body: web::Bytes) -> impl Responder {
+    let body = String::from_utf8(body.to_vec()).unwrap();
+    let neko_text = body.split(" ");
+    let mut translated = String::new();
+    for word in neko_text {
+        translated.push_str(&format!("{}-nya ", word));
+    }
+    format!("Translated: {translated}")
+}
+
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -17,6 +28,7 @@ async fn main() -> std::io::Result<()> {
             .route("/hello", web::get().to(|| async { "Hello World!" }))
             .service(home)
             .service(greet)
+            .service(translate)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
